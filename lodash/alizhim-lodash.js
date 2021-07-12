a92c1cd02c15066bc9e3907cd1549557b660ecb2
alizhim <291436347@qq.com>
2021-07-12 16:06:13 +0800

    hfiashfd

--- lodash/alizhim-lodash.js
+++ lodash/alizhim-lodash.js
@@ -1,11 +1,11 @@
 var damiao = function () {
-  function forEach(array, fn) {
+  function foreach(array, fn) {
     for (var i = 0; i < array.length; i++) {
       fn(array[i], i, array)
     }
     return array
   }
   return {
-    forEach: forEach,
+    foreach: foreach,
   }
 }
