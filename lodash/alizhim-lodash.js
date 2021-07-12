e341e313c990c0243581cc4a99c42cbb1b4f3fbd
alizhim <291436347@qq.com>
2021-07-12 16:34:50 +0800

    sdlhfuthg

--- lodash/alizhim-lodash.js
+++ lodash/alizhim-lodash.js
@@ -5,7 +5,22 @@ var alizhim = function () {
     }
     return array
   }
+  function concat(arr,...arg) {
+    let result = arr;
+    for(let item of arg){
+        if(Array.isArray(item)){
+            for(let subitem of item){
+                result.push(subitem);
+            }
+        }else{
+            result.push(item);
+        }
+    }
+    return result;
+}
+module.exports = concat;
   return {
     forEach: forEach,
+    concat: concat
   }
 }
