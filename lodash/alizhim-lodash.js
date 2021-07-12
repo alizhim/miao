05b89677bd1feed427a18cc14938d55e6755ea5a
alizhim <291436347@qq.com>
2021-07-12 15:55:14 +0800

    dgfag

---
+++ lodash/alizhim-lodash.js
@@ -0,0 +1,11 @@
+var damiao = function () {
+  function forEach(array, fn) {
+    for (var i = 0; i < array.length; i++) {
+      fn(array[i], i, array)
+    }
+    return array
+  }
+  return {
+    forEach: forEach,
+  }
+}
