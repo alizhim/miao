22d60b79e98ea181c948769b2e78c92b5106a300
alizhim <291436347@qq.com>
2021-07-22 22:03:12 +0800

    hdfhdhfhf

--- lodash/alizhim-lodash.js
+++ lodash/alizhim-lodash.js
@@ -95,6 +95,62 @@ var alizhim = function () {
     }
     return baseFill(array, value, start, end);
   }
+
+  function slice(array, start, end) {
+    let length = array == null ? 0 : array.length
+     // 如果array为null，length为0，否则取array.length
+    if (!length) {
+      return []
+    }
+     // 如果length为0，则返回空数组
+    start = start == null ? 0 : start
+    // 判断第二个参数start有没有传值，没有传值则取0，传值则取传入的值
+    end = end == null ? 0 : end
+     // 判断第三个参数end有没有传值，没有传值则取array的长度，传值则取传入的值
+    if (start < 0) {
+      start = -start > length ? 0 : (length + start)
+    }
+    // 处理start小于0的情况
+    // 如果start小于0且-start的值大于array的长度，则start取0，否则start取数组长度加start的值
+    end = end > length ? length : end
+     // 当end值大于数组长度时，end取数组的长度
+    if (end < 0) {
+      end += length
+    }
+    // 处理end小于0的情况
+    // 当end小于0时，end取数组长度加end的值
+    length = start > end ? 0 : ((end - start) >>> 0)
+    // 如果start大于end，则length取0，否则取end-start值
+    // 其中(end-start) >>> 0作用是保证(end-start)是一个有意义的正整数，详细可参考这篇文章：https://segmentfault.com/a/1190000014613703
+    // 执行下面语句后，length值将变为返回数组的长度
+    start >>= 0
+    // 确保start为一个有意义的正整数
+    let index = -1
+    // 定义一个index，后面将作为数组result的索引值
+    const result = new Array(length)
+     // 定义返回数组result，其长度为length
+    while (++index < length) {
+      result[index] = array[index + start]
+    }
+    // 遍历数组result，并将原数组array中index+start对应的元素添加到result数组中index对应的位置
+    return result
+  }
+  function indexOf(array, value, fromIndex = 0) {
+    for (let i = fromIndex ; i < array.length; i++) {
+      if (array[i] === value) {
+        return i
+      }
+    }
+    return -1
+  }
+  function lastIndexOf(array, value, fromIndex = array.length - 1) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (array[i] === value) {
+        return i
+      }
+    }
+    return -1
+  }
   return {
     forEach: forEach,
     concat: concat,
@@ -105,6 +161,9 @@ var alizhim = function () {
     some: some,
     sum: sum,
     chunk: chunk,
-    fill: fill
+    fill: fill,
+    slice: slice
+    indexOf: indexOf,
+    lastIndexOf: lastIndexOf,
   }
 }()
